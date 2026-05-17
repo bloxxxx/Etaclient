013533257ac184a7bb7a530a8f08230d0492906a
bloxxxx <127309012+bloxxxx@users.noreply.github.com>
2026-05-17 13:22:21 +0200

    Remade VarItem parsing using JsonUtil

--- src/main/java/io/github/bloxxxx/etaclient/hypercube/plot/varitem/VarItem.java
+++ src/main/java/io/github/bloxxxx/etaclient/hypercube/plot/varitem/VarItem.java
@@ -1,30 +1,23 @@
 package io.github.bloxxxx.etaclient.hypercube.plot.varitem;
 
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
+import io.github.bloxxxx.etaclient.util.JsonUtil;
 import io.github.bloxxxx.etaclient.util.PBVUtil;
 import net.minecraft.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
-//TODO: Remake using JsonUtil
+
 public interface VarItem {
     static @Nullable VarItem parse(JsonObject root) {
         if (root == null) return null;
 
-        JsonElement idElement = root.get("id");
-        if (idElement == null) return null;
-        if (!idElement.isJsonPrimitive()) return null;
-        JsonPrimitive idPrimitive = idElement.getAsJsonPrimitive();
-        if (!idPrimitive.isString()) return null;
-        String id = idElement.getAsString();
+        String id = JsonUtil.getAsString(root, "id");
+        if (id == null) return null;
 
         VarItemType type = VarItemType.fromId(id);
         if (type == null) return null;
 
-        JsonElement dataElement = root.get("data");
-        if (dataElement == null) return null;
-        if (!dataElement.isJsonObject()) return null;
-        JsonObject data = dataElement.getAsJsonObject();
+        JsonObject data = JsonUtil.getAsJsonObject(root, "data");
+        if (data == null) return null;
 
         return type.construct(data);
     }
